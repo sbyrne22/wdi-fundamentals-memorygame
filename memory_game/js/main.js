3b2fb24272f5bdeba26dd1f0aed28324d4d0ec8e
Sean Byrne <spickles@Seans-iMac.local>
2017-10-19 17:37:40 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,10 +21,13 @@ var cards = [
 		cardImage: "images/king-of-diamonds.png"
 	}
 ];
-
+//var cardElement = []; // This way works as well, as long as they're var'd in the function
+//var cardId = []; 		// This way works as well, as long as they're var'd in the function
 var cardsInPlay = [];
 
 var checkForMatch = function () {
+	//this.setAttribute('src', cards[cardId].cardImage);
+	//cardElement.setAttribute('src', cards[cardId].cardImage); // No Errors, but only changes last picture
 	if (cardsInPlay.length === 2){
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
@@ -32,25 +35,27 @@ var checkForMatch = function () {
 			alert("Sorry, try again.");
 		}
 	}
-}
+};
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id'); //correct
 	console.log('user flipped ' + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
-}
+};
 
 var createBoard = function () {
 	for (var i = 0; i < cards.length; i++) {
-		var cardElement = document.createElement('img');
-		cardElement.setAttribute('src', 'images/back.png');
-		cardElement.setAttribute('data-id', 'i');
-		cardElement[i].addEventListener('click', flipCard);
-		cardElement[0].appendChild('#game-board');
+		var cardElement = document.createElement('img'); 	//correct
+		cardElement.setAttribute('src', 'images/back.png'); //correct
+		cardElement.setAttribute('data-id', i);				// I Think Correct
+		cardElement.addEventListener('click', flipCard);	//correct
+		document.getElementById('game-board').appendChild(cardElement); //correct
 	}
-}
+};
 
 createBoard();
 
